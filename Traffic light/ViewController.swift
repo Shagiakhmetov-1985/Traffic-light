3797cefb038c67a9c8850b76eb6d744f101b2acb
Marat Shagiakhmetov <shagiakhmetov2499@gmail.com>
2021-03-24 16:10:47 +0300

    Errors fixed

--- Traffic light/ViewController.swift
+++ Traffic light/ViewController.swift
@@ -15,27 +15,31 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         buttonNext.layer.cornerRadius = 15
-        redView.backgroundColor = #colorLiteral(red: 0.2638142705, green: 0.01383866463, blue: 0, alpha: 1)
-        yellowView.backgroundColor = #colorLiteral(red: 0.3098039329, green: 0.2039215714, blue: 0.03921568766, alpha: 1)
-        greenView.backgroundColor = #colorLiteral(red: 0.1294117719, green: 0.2156862766, blue: 0.06666667014, alpha: 1)
+        
+        redView.alpha = 0.3
+        yellowView.alpha = 0.3
+        greenView.alpha = 0.3
+        
         redView.layer.cornerRadius = 75
         yellowView.layer.cornerRadius = 75
         greenView.layer.cornerRadius = 75
         
     }
     @IBAction func actionButtonNext(_ sender: UIButton) {
-        if redView.backgroundColor == #colorLiteral(red: 0.2638142705, green: 0.01383866463, blue: 0, alpha: 1) && yellowView.backgroundColor == #colorLiteral(red: 0.3098039329, green: 0.2039215714, blue: 0.03921568766, alpha: 1) && greenView.backgroundColor == #colorLiteral(red: 0.1294117719, green: 0.2156862766, blue: 0.06666667014, alpha: 1) {
-            redView.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
-        } else if redView.backgroundColor == #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1) {
-            redView.backgroundColor = #colorLiteral(red: 0.2638142705, green: 0.01383866463, blue: 0, alpha: 1)
-            yellowView.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
-        } else if yellowView.backgroundColor == #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1) {
-            yellowView.backgroundColor = #colorLiteral(red: 0.3098039329, green: 0.2039215714, blue: 0.03921568766, alpha: 1)
-            greenView.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
-        } else if greenView.backgroundColor == #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1) {
-            greenView.backgroundColor = #colorLiteral(red: 0.1294117719, green: 0.2156862766, blue: 0.06666667014, alpha: 1)
-            redView.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+    
+        if redView.alpha < 1 && yellowView.alpha < 1 && greenView.alpha < 1 {
+            redView.alpha = 1
+        } else if redView.alpha == 1 {
+            yellowView.alpha = 1
+            redView.alpha = 0.3
+        } else if yellowView.alpha == 1 {
+            greenView.alpha = 1
+            yellowView.alpha = 0.3
+        } else if greenView.alpha == 1 {
+            redView.alpha = 1
+            greenView.alpha = 0.3
         }
     }
 
